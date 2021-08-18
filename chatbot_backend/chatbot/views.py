77898b44038600bb742ef88c8f7841c6bf426f68
unknown <huan.la54066034@hcmut.edu.vn>
2021-08-18 20:36:29 +0700

    build: upload image

--- chatbot_backend/chatbot/views.py
+++ chatbot_backend/chatbot/views.py
@@ -1,13 +1,26 @@
+from django.http import response
 from django.http.response import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from chatbot.process import intent, getMessStack
+
+import io
+import cv2
+import numpy as np
+from PIL import Image
 
 # Create your views here.
 @csrf_exempt
 def answer(request):
     if request.method == 'POST':
         message = request.POST['message']
+        chunks = request.FILES['img'].chunks()
+        data = next(chunks)
+        image = Image.open(io.BytesIO(data))
+        image = np.array(image)
+        img = cv2.resize(image, (100, 100))
+        img = Image.fromarray(img, 'RGB')
+        img.show()
         intent(message)
         return JsonResponse({})
     elif request.method == 'GET':
--- chatbot_frontend/src/components/ChatFeed.js
+++ chatbot_frontend/src/components/ChatFeed.js
@@ -6,11 +6,63 @@ import './components.css';
 class ChatFeed extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {message: [], input: ''};
+        this.state = {message: [], input: '', file: null};
         this.check = 'check';
+        this.messFormHeight = '60px';
         this.processInput = this.processInput.bind(this);
         this.processMessage = this.processMessage.bind(this);
         this.handleResponse = this.handleResponse.bind(this);
+        this.fileDisplay = this.fileDisplay.bind(this);
+        this.fileRemove = this.fileRemove.bind(this);
+    }
+
+    fileDisplay() {
+        if (this.state.file === null) {
+            return (null);
+        }
+        else {
+            const url = URL.createObjectURL(this.state.file);
+            const but_style = {
+                height: '17px',
+                width: '17px',
+                position: 'absolute',
+                top: '5px',
+                left: '80px',
+                border: '1px solid gray',
+                borderRadius: '17px',
+                textAlign: 'center',
+                fontSize: '13px',
+            };
+            return (
+                <div>
+                    <div style={{height:'80px', width:'80px', position: 'absolute', bottom: "60px", left: "10px"}}>
+                        <img src={url} alt='Img' style={{height: '100%', width: '100%', objectFit: 'cover'}} />
+                    </div>
+                    <div className='del-button' style={but_style} onClick={this.fileRemove}> x </div>
+                </div>
+            );
+        }
+    }
+
+    fileRemove() {
+        const state = this.state;
+        state.file = null;
+        this.setState(state);
+        this.messFormHeight = '60px';
+    }
+
+    handleFileSelect = (e) => {
+        e.preventDefault();
+        const fileSelector = document.createElement('input');
+        fileSelector.setAttribute('type', 'file');
+        fileSelector.setAttribute('accept', 'image/png, image/jpeg, image/jpg');
+        fileSelector.onchange = (event) => {
+            this.messFormHeight = '150px';
+            const state = this.state;
+            state.file = event.target.files[0];
+            this.setState(state);
+        };
+        fileSelector.click();
     }
 
     componentDidMount() {
@@ -58,35 +110,41 @@ class ChatFeed extends React.Component {
         }
     }
 
-    processMessage() {
-        if (this.state.input.length > 0) {
+    processMessage(e) {
+        e.preventDefault();
+        if (this.state.input.length > 0 || this.state.file !== null) {
             let state = this.state;
-            state.message.push(<div className='message'>
-                    <div className="uncheck" style={{
-                        height: "12px",
-                        width: "12px",
-                        position: "absolute",
-                        bottom: "8px",
-                        right: "8px",
-                        borderRadius: "12px",
-                        border: "1px solid lightgray"
-                    }}>
-                        <span className={this.check} style={{right: "30%", top: "10%", position: "absolute"}} />
-                    </div>
+            if (state.input.length > 0) {
+                state.message.push(<div className='message'>
+                        <div className="uncheck" style={{
+                            height: "12px",
+                            width: "12px",
+                            position: "absolute",
+                            bottom: "8px",
+                            right: "8px",
+                            borderRadius: "12px",
+                            border: "1px solid lightgray"
+                        }}>
+                            <span className={this.check} style={{right: "30%", top: "10%", position: "absolute"}} />
+                        </div>
 
-                    <div className='mymessage'>
-                        {state.input} 
-                    </div>
-                    <div style={{clear:'both'}}></div> 
-                </div>  
-            );
-            
+                        <div className='mymessage'>
+                            {state.input} 
+                        </div>
+                        <div style={{clear:'both'}}></div> 
+                    </div>  
+                );
+            }
             const data = new FormData();
             data.append('message', state.input);
+            data.append('img', state.file);
+
+            console.log(data.get('file'));
 
             axios.post(
                 'http://127.0.0.1:8000/chatbot/answer',
                 data,
+                {headers: {"content-type": "multipart/form-data"}}
             ).then((response) => {
                 if (response.status === 200) {
                     const check_icon = document.getElementsByClassName('uncheck');
@@ -98,7 +156,9 @@ class ChatFeed extends React.Component {
             });
 
             state.input = '';
-            this.setState(state);  
+            state.file = null;
+            this.messFormHeight = '60px';
+            this.setState(state);
         }
     }
 
@@ -142,56 +202,63 @@ class ChatFeed extends React.Component {
         );
     }
 
-    MessageForm() {
+    MessageForm(height) {
         const style = {
             width: "100%",
-            height: "60px",
-            borderTop: "1px solid lightgray"
+            height: height,
+            borderTop: "1px solid lightgray",
+            position: 'relative',
         };
     
         const tool_style = {
             width: "150px",
             height: "100%",
             float: "left",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
+            position: 'relative',
         };
     
         const input_style = {
             height: "100%",
             overflow: "auto",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
+            position: 'relative',
         };
     
         const but_style = {
             width: "50px",
             height: "100%",
             float: "right",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+            position: 'relative',
         }
     
         return (
             <div style={style}>
                 <div style={tool_style}>
-                    <button className='button-tool' style={{marginRight: "10px", backgroundImage: "url(http://localhost:3000/images/file_icon.jpg)", backgroundSize: "cover"}} />
-                    <button className='button-tool' style={{marginLeft: "10px", backgroundImage: "url(http://localhost:3000/images/emotion_icon.jpg)", backgroundSize: "cover"}} />
+                    <button className='button-tool' onClick={this.handleFileSelect} 
+                        style={{marginRight: "10px", 
+                            backgroundImage: "url(http://localhost:3000/images/file_icon.jpg)", 
+                            backgroundSize: "cover",
+                            position: 'absolute', bottom: "10px", left: "15%" }} />
+                    <button className='button-tool' 
+                        style={{marginLeft: "10px", 
+                            backgroundImage: "url(http://localhost:3000/images/emotion_icon.jpg)", 
+                            backgroundSize: "cover",
+                            position: 'absolute', bottom: "10px", right: "15%"}} />
                 </div>
                 
                 <div style={but_style}>
-                    <button className='button-tool' style={{backgroundImage: "url(http://localhost:3000/images/send_icon.jpg)", backgroundSize: "cover"}} onClick={this.processMessage} />
+                    <button className='button-tool' 
+                        style={{backgroundImage: "url(http://localhost:3000/images/send_icon.jpg)", backgroundSize: "cover",
+                        position: 'absolute', bottom: "10px", left: "10%" }} onClick={this.processMessage} />
                 </div>
                 
                 <div style={input_style}>
-                    <div style={{width: "98%", height:"60%", border: "none"}}>
+                    {this.fileDisplay()}
+                    <div style={{height:"35px", width: "98%", paddingRight: '10px', border: "none", position: 'absolute', bottom: "12px", left: "10px"}}>
                         <input type="text" value={this.state.input} placeholder="Aa" 
-                            style={{width:"100%", height:"100%", borderRadius: "20px", border: "none", textIndent: "15px", fontSize: "18px"}} 
+                            style={{width:"100%", height:"100%", borderRadius: "20px",
+                                border: "none", textIndent: "15px", fontSize: "18px"}}
                             onChange={this.processInput} onSubmit={this.processMessage} 
-                            onKeyDown={(e) => {if (e.key === 'Enter') {this.processMessage();}}}/>
+                            onKeyDown={(e) => {if (e.key === 'Enter') {this.processMessage(e);}}}/>
                     </div>
                 </div>
             </div>
@@ -203,7 +270,7 @@ class ChatFeed extends React.Component {
             <div className="chat-feed-container">
                 {this.FeedTopic()}
                 {this.FeedMessage()}
-                {this.MessageForm()}
+                {this.MessageForm(this.messFormHeight)}
             </div>
         );
     }
